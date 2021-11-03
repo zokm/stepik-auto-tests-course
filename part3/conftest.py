29ecfeb0bbd592a8b2583f0549aa1a548e83bd48
Vladimir <vaavik@bk.ru>
2021-11-03 12:07:05 +0900

    change file for run from consol use different browsers

--- part3/conftest.py
+++ part3/conftest.py
@@ -2,8 +2,19 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help="Choose_browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    driver = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    driver = None
+    if browser_name == 'chrome':
+        driver = webdriver.Chrome()
+    elif browser_name == 'firefox':
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield driver
     driver.quit()
