78fd19bf550a5cfda3779291531d059a7e8fc4af
Vladimir <vaavik@bk.ru>
2021-11-04 13:39:46 +0900

    added parser for languages

--- rerun/conftest.py
+++ rerun/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', help="Choose_browser: chrome or firefox")
-    parser.addoption('--language', action='store', default='None', help="Choose language: en, ru")
+    parser.addoption('--language', action='store', default='None', help="Choose_language: en, ru")
 
 
 @pytest.fixture(scope="function")
